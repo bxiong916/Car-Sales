ce6ec36bf5eb164401842801827a32d2f1289295
Bill Xiong <xiong.bill@yahoo.com>
2020-11-14 15:43:46 -0800

    MVP

--- src/index.js
+++ src/index.js
@@ -5,5 +5,18 @@ import App from './App';
 import 'bulma/css/bulma.css';
 import './styles.scss';
 
+import {Provider} from "react-redux";
+import {createStore} from "redux";
+import {carsReducer} from "./reducer";
+
+const store = createStore(carsReducer)
+
 const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>, 
+    rootElement
+);
